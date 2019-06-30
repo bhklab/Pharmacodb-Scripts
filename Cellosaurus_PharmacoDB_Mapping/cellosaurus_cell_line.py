af63c969b3830faecfda7d656f4df3f290a78c5f
gangeshberi <gangeshberi1@gmail.com>
2019-06-30 03:30:45 -0400

    added code to find the replicates

--- Cellosaurus_PharmacoDB_Mapping/cellosaurus_cell_line.py
+++ Cellosaurus_PharmacoDB_Mapping/cellosaurus_cell_line.py
@@ -10,7 +10,9 @@ pharmacodb_file = 'cell_line_pharmacodb.csv'
 cellosaurus_file = 'cellosaurus_cell_line.csv'
 output_file_cellosaurus = './output_files/output_cellosaurus.csv'
 output_file_pharmacodb = './output_files/output_pharmacodb.csv'
+output_file_duplicate = './output_files/output_duplicate.csv'
 current_year = (datetime.date.today()).strftime('%Y')
+
 
 
 # function will read through the file and store the results in unique cell line list.
@@ -31,6 +33,7 @@ def readFile(filename):
                     cellosaurus_cell_line.append(data)
 
 
+
 # function will write the output to the new csv file.
 def writeFileCellosauruss(filename):
     with open(filename, 'w') as csv_file:
@@ -39,7 +42,7 @@ def writeFileCellosauruss(filename):
         for row in pharmacodb_cell_line:
             data = row.split('_')[0]
             pharmacodb_id = row.replace('-','')
-            synonym = ''
+            accession_id = ''
             total_number = 0
             for row in cellosaurus_cell_line:
                 sy = row['sy'].split('; ')
@@ -50,15 +53,42 @@ def writeFileCellosauruss(filename):
                 elif(total_number == cellosaurus_cell_line.__len__() and data in sy):
                     csv_writer.writerow([row['accession'], pharmacodb_id, data])
                 elif(data in sy):
-                    synonym = row['accession']
+                    accession_id = row['accession']
                 elif(total_number == cellosaurus_cell_line.__len__()):
-                    csv_writer.writerow([synonym, pharmacodb_id, data])
-                
-                
+                        csv_writer.writerow([accession_id, pharmacodb_id, data])
+
+
+
+# function to find the duplicate cell line names whether 
+# it's the primary cell id or synonyms in Cellosaurus file.    
+def duplicateCellLines(filename):
+    with open(filename, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter = ',')
+        csv_writer.writerow(['cell_line', "total_repetitions"])
+        for row in pharmacodb_cell_line:
+            data = row.split('_')[0]
+            total_number = 0
+            replicates = 0
+            for row in cellosaurus_cell_line:
+                sy = row['sy'].split('; ')
+                total_number = total_number + 1
+                if((row['identifier'] == data) or (data in sy)):
+                    replicates = replicates + 1
+                elif(total_number == cellosaurus_cell_line.__len__()):
+                    if(replicates > 1):
+                        csv_writer.writerow([data, str(replicates)])
+
+
+
 # this will loop through the files to read in the folder.
 # it contains pharmacodb cell line file as well as from cellosaurus.
 for entry in glob('input_files/*.csv'):
     readFile(entry)
     
+
 # calling write function.
-writeFileCellosauruss(output_file_cellosaurus)
+#writeFileCellosauruss(output_file_cellosaurus)
+
+
+# calling function to find duplicates.
+duplicateCellLines(output_file_duplicate)
