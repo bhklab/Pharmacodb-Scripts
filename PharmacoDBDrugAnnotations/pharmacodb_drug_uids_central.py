5c103948e73f4971fef6e347ecd75f99d5ed2e32
gangeshberi <gangeshberi@gmail.com>
2020-11-10 15:57:34 -0500

    script to create the UIDs for compounds in the central compound data file.

---
+++ PharmacoDBDrugAnnotations/pharmacodb_drug_uids_central.py
@@ -0,0 +1,30 @@
+import csv
+import re
+
+# input and output file variables.
+inputFile = 'drugs_with_ids.csv'
+outputFile = 'drugsUIDCentral.csv'
+ACRONYM = 'PDBC'
+
+# try catch block for reading and writing to the file.
+try:
+    # opening the file.
+    with open(inputFile, 'r', encoding='utf-8') as input:
+        with open(outputFile, 'w', encoding='utf-8') as output:
+            csv_writer = csv.writer(output)
+            first_line = True
+            i = 1
+            # looping through each line in the file
+            # and appending the result to new file.
+            for line in input:
+                if first_line:
+                    line = line.replace('\n', '') + ',' + 'PharmacoDB.uid'
+                    output.write(line + '\n')
+                    first_line = False
+                else:
+                    drug_uid = ACRONYM + format(i, '05d')
+                    line = line.replace('\n', '') + ',' + drug_uid
+                    output.write(line + '\n')
+                    i += 1
+except:
+    print('Something went wrong!!')
