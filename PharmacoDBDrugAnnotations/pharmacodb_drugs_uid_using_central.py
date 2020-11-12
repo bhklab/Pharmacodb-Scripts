5f7dd008d8552473288a4b1fcb0d56f97ae7327e
gangeshberi <gangeshberi@gmail.com>
2020-11-12 11:05:47 -0500

    changes to the script.

--- PharmacoDBDrugAnnotations/pharmacodb_drugs_uid_using_central.py
+++ PharmacoDBDrugAnnotations/pharmacodb_drugs_uid_using_central.py
@@ -10,4 +10,61 @@ pharmacodb_input_drugs = 'drugs.csv'
 # input central file with the drug UIDs.
 main_input_drugs = 'drugsUIDCentral.csv'
 # output file.
-outputFile = "drugsUID.csv"
+output_file = "drugsUID.csv"
+
+# dictionary to store the drug and uid mapping.
+drug_uid_mapping = {}
+
+# column number/index that have to be mapped.
+column = [1, 3, 5, 6, 8, 10, 11, 12, 14, 16, 20, 21]
+
+
+# try except block.
+try:
+    # creates the mapping object.
+    # puts the drugs from several datasets in the mapping.
+    with open(main_input_drugs, 'r') as input:
+        for line in csv.reader(input):
+            for i in column:
+                if '///' in line[i]:
+                    drug = line[i].split('///')
+                    for j in drug:
+                        drug_uid_mapping[j] = line[-1]
+                else:
+                    drug_uid_mapping[line[i]] = line[-1]
+
+    # drug_uid_mapping[line[1]] = line[-1]
+    #         drug_uid_mapping[line[3]] = line[-1]
+    #         drug_uid_mapping[line[5]] = line[-1]
+    #         drug_uid_mapping[line[6]] = line[-1]
+    #         drug_uid_mapping[line[8]] = line[-1]
+    #         drug_uid_mapping[line[10]] = line[-1]
+    #         drug_uid_mapping[line[11]] = line[-1]
+    #         drug_uid_mapping[line[12]] = line[-1]
+    #         drug_uid_mapping[line[14]] = line[-1]
+    #         drug_uid_mapping[line[16]] = line[-1]
+    #         drug_uid_mapping[line[20]] = line[-1]
+    #         drug_uid_mapping[line[21]] = line[-1]
+
+    # generating the output file.
+    with open(pharmacodb_input_drugs, 'r') as input_drugs:
+        with open(output_file, 'w') as output:
+            csv_writer = csv.writer(output, delimiter=',')
+            # header for the output file.
+            csv_writer.writerow(['drug_id', 'drug_name', 'drug_uid',
+                                 'fda_status', 'created_at', 'updated_at'])
+            # looping through each line in the file
+            # and appending the result to new file.
+            for line in input_drugs:
+                line = line.split(';')
+                drug = line[1].replace('"', '')
+                if line[0] != 'drug_id' and drug in drug_uid_mapping:
+                    drug_uid = drug_uid_mapping[drug]
+                    csv_writer.writerow(
+                        [line[0], line[1], drug_uid, line[2], line[3], line[4].replace('\n', '')])
+                else:
+                    print(line[1])
+
+except:
+    print('Something went wrong!!')
+    raise
