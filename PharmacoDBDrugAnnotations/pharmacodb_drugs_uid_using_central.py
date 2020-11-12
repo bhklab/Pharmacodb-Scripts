25b2cc098df3f0d6e4d5f0717886a711b8158d07
gangeshberi <gangeshberi@gmail.com>
2020-11-12 13:06:12 -0500

    added qoute char.

--- PharmacoDBDrugAnnotations/pharmacodb_drugs_uid_using_central.py
+++ PharmacoDBDrugAnnotations/pharmacodb_drugs_uid_using_central.py
@@ -1,5 +1,5 @@
 """
-This file is used to create the UIDs for compounds that are present in PharmacoDB 
+This file is used to create the UIDs for compounds that are present in PharmacoDB
 using the UIDs created/present in the drugsUIDCentral.csv file.
 """
 import csv
@@ -45,7 +45,7 @@ try:
     # generating the output file.
     with open(pharmacodb_input_drugs, 'r') as input_drugs:
         with open(output_file, 'w') as output:
-            csv_writer = csv.writer(output, delimiter=',')
+            csv_writer = csv.writer(output, delimiter=',', quotechar="'")
             # header for the output file.
             csv_writer.writerow(header)
             # looping through each line in the file
@@ -55,12 +55,13 @@ try:
                 drug = line[1].replace('"', '').strip()
                 if line[0] != 'drug_id' and drug in drug_uid_mapping:
                     drug_uid = drug_uid_mapping[drug]
+                    last = line[4].replace('\n', '')
                     csv_writer.writerow(
-                        [line[0], line[1], drug_uid, line[2], line[3], line[4].replace('\n', '')])
+                        [line[0], line[1], drug_uid, line[2], line[3], last])
                 elif drug.capitalize() in drug_uid_mapping:
                     drug_uid = drug_uid_mapping[drug.capitalize()]
                     csv_writer.writerow(
-                        [line[0], line[1], drug_uid, line[2], line[3], line[4].replace('\n', '')])
+                        [line[0], line[1], drug_uid, line[2], line[3], last])
                 else:
                     print('This compound was not mapped!', line[1])
 except:
