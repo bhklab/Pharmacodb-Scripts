046f4e0db4f3bf4880097338502e196485f65390
gangeshberi <gangeshberi@gmail.com>
2020-11-25 14:01:31 -0500

    updated the code and uid file to include an object to map the missing UIDs.

--- PharmacoDBDrugAnnotations/pharmacodb_drugs_uid_using_central.py
+++ PharmacoDBDrugAnnotations/pharmacodb_drugs_uid_using_central.py
@@ -3,7 +3,7 @@ This file is used to create the UIDs for compounds that are present in PharmacoD
 using the UIDs created/present in the drugsUIDCentral.csv file.
 Note: Nelfivir is mapped using the drug name Nelfinavir
       and Lisitinib to Linsitinib. These both are mapped manually.
-      Also replace OSI-906 with 'NA'.
+      Also map OSI-906 for now.
 """
 
 import csv
@@ -16,17 +16,24 @@ main_input_drugs = 'drugsUIDCentral.csv'
 # output file.
 output_file = "drugsUID.csv"
 
+# dictionary to map the wrong named drugs.
+missing_drugs = {
+    "Nelfivir": "PDBC49359",
+    "Lisitinib": "PDBC00096",
+    "OSI-906": "PDBC00096-R",
+    "pazopanib": "PDBC00102-R",
+    "tipifarnib": "PDBC00106-R",
+}
+
+
 # dictionary to store the drug and uid mapping.
 drug_uid_mapping = {}
-
-# dictionary to map unique.drugid and PharmacoDB.uid.
-drug_unique_id_mapping = {}
 
 # list to maintained already used PharmacoDB.uids.
 pharmacodb_uids = []
 
 # column number/index that have to be mapped.
-column = [3, 5, 6, 8, 10, 11, 12, 14, 16, 20, 21]
+column = [1, 3, 5, 6, 8, 10, 11, 12, 14, 16, 20, 21]
 
 # header for the new file.
 header = ['drug_id', 'drug_name', 'drug_uid',
@@ -49,8 +56,6 @@ try:
     # puts the drugs from several datasets in the mapping.
     with open(main_input_drugs, 'r') as input:
         for line in csv.reader(input):
-            # mapping unique ids to pharmacodb uids.
-            drug_unique_id_mapping[line[1]] = line[-1]
             for i in column:
                 map_drug_to_uid(i, line)
 
@@ -62,30 +67,27 @@ try:
             # and appending the result to new file.
             for line in input_drugs:
                 line = line.split(';')
-                drug = line[1].replace('"', '').strip()
                 drug_uid = ''
-                last = last = line[4].replace('\n', '')
+                drug = line[1].replace('"', '').strip()
+                last = line[4].replace('\n', '')
                 if line[0] == 'drug_id':
                     drug_uid = 'drug_uid'
-                elif drug in drug_unique_id_mapping:
-                    drug_uid = drug_unique_id_mapping[drug]
-                    pharmacodb_uids.append(drug_uid)
-                elif drug.capitalize() in drug_unique_id_mapping:
-                    drug_uid = drug_unique_id_mapping[drug.capitalize()]
-                    pharmacodb_uids.append(drug_uid)
+                elif drug in missing_drugs:
+                    drug_uid = missing_drugs[drug]
                 elif drug in drug_uid_mapping:
                     drug_uid = drug_uid_mapping[drug]
-                    if(drug_uid in pharmacodb_uids):
-                        drug_uid = drug_uid + '-R'
-                    pharmacodb_uids.append(drug_uid)
                 elif drug.capitalize() in drug_uid_mapping:
                     drug_uid = drug_uid_mapping[drug.capitalize()]
-                    if(drug_uid in pharmacodb_uids):
-                        drug_uid = drug_uid + '-R'
-                    pharmacodb_uids.append(drug_uid)
                 else:
                     print('This compound was not mapped!', line[1])
                     drug_uid = 'NA'
+                # if the uid is already present or used then add an -R to it.
+                if(drug_uid in pharmacodb_uids):
+                    drug_uid = drug_uid + '-R'
+                # appending uids to the list.
+                else:
+                    pharmacodb_uids.append(drug_uid)
+                # write to the csv file.
                 csv_writer.writerow(
                     [line[0], line[1], drug_uid, line[2], line[3], last])
 
